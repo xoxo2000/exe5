5d150fa9ec0a8ebc4f681489cbebe17966902e51
xoxo2000 <m338739@gmail.com>
2025-07-26 11:34:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0) {
+        basic.showIcon(IconNames.Happy)
+    } else if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Sad)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(4)
+basic.forever(function () {
+	
+})
